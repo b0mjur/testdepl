12560471abc813b246e58eda088a5f30e7af06b7
b0mjur <vitalikkorolyshyn@gmail.com>
2025-04-05 20:01:55 -0700

    Update key pair main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  key_name      = "devops2"
+  key_name      = "devops1"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
