c86ca1c81052162e5c9be6a55516a2b393faa595
b0mjur <vitalikkorolyshyn@gmail.com>
2025-04-07 17:12:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,13 @@ resource "aws_instance" "web_server" {
 
   user_data = <<-EOF
               #!/bin/bash
+              amazon-linux-extras install epel
               yum update -y
+              mkdir -p /var/lib/swap
+              dd if=/dev/zero of=/var/lib/swap/swapfile bs=1M count=1024
+              chmod 0600 /var/lib/swap/swapfile
+              mkswap /var/lib/swap/swapfile
+              swapon /var/lib/swap/swapfile
               PUBLIC_IP=$(curl -s https://api.ipify.org)
               curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/vitalii-ansible-task/buildWithParameters?token=Abc123456&MANAGED_HOST=$PUBLIC_IP"
               EOF
